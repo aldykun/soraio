8cdc3656ee5fc6a5d2a30b03570573a3c62a7cab
lowsprofile <zdumb1885@outlook.com>
2016-07-30 17:45:20 +0700

    site conf dummy added

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ var conf = require('../lib/conf');
 // show the login form
 router.get('/login', function(req, res, next) {
   // render the page and pass in any flash data if it exists
-  res.render('login', { title: 'Log In', message: req.flash('loginMessage'), layout: 'auth-layout', site: conf);
+  res.render('login', { title: 'Log In', message: req.flash('loginMessage'), layout: 'auth-layout', site: conf});
 });
 
 // process the login form
